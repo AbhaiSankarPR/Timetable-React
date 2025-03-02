0b0375784c21693fbab0b848b18e5e5c68149c07
Abhai Sankar P R <abhaisankarprofficial@gmail.com>
2025-03-02 14:02:00 +0530

    Update Firebase.jsx

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -2,16 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBrgdWlI10a2zC6yBRyvw2YttXv81WMFRU",
-  authDomain: "timetable-react.firebaseapp.com",
-  databaseURL: "https://timetable-react-default-rtdb.firebaseio.com",
-  projectId: "timetable-react",
-  storageBucket: "timetable-react.firebasestorage.app",
-  messagingSenderId: "781063761197",
-  appId: "1:781063761197:web:f8f07bf7615f896e0977a4",
-  measurementId: "G-SZ9KQHTRXK"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
-
-export const auth=getAuth();
+export const auth = getAuth(app);
