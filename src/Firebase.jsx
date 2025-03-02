8546caa70cdc0d510cb629d2aa7ff9e3a5449ab6
Abhai Sankar P R <abhaisankarprofficial@gmail.com>
2025-03-02 14:18:33 +0530

    Update Firebase.jsx

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -2,8 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  apiKey: import.meta.env.VITE_FIREBASE_,
+  authDomain: import.meta.env.VITE_FIREBASE_DOMAIN,
   databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
